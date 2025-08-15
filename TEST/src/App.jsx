c7de6aad41285afc19a80564b81e08466cb46c3b
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2025-08-15 23:09:47 +0300

    feat: adding frontend

--- TEST/src/App.jsx
+++ TEST/src/App.jsx
@@ -2,27 +2,63 @@ import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
+
+    const mainStyle = {backgroundColor: '#2a3441', minHeight: '100vh', padding: '1.3rem',fontFamily: 'Arial'};
+    const tableStyle = {backgroundColor: '#3c4a59',borderRadius: '8px',borderCollapse: 'separate',borderSpacing: '0',width: '100%',maxWidth: '800px',overflow: 'hidden'};
+    const theadTrStyle = {backgroundColor: '#4a5864'}
+    const theadThStyle = {color: '#a0b0c0',padding: '12px 16px',textAlign: 'left',fontSize: '14px',fontWeight: 'normal'}
+    const tbodytdStyle = {color: '#e0e6ed',padding: '12px 16px',fontSize: '14px'}
+
+//---------------------------STYLE------------------------------------------
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        const loadCharacters = async () => {
+            try{
+                const response = await fetch('characters.json');
+                const jsonData = await response.json();
+                setCharacters(jsonData.characters)
+            }
+            catch(err) {
+                console.error("Error: ",err);
+            }
+        }
+        loadCharacters();
+    }, [])
+
     return (
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>RealName</th>
-                        <th>Universe</th>
+        <main style={mainStyle}>
+            <section>
+                <table style={tableStyle}>
+                    <thead>
+                    <tr style={theadTrStyle}>
+                        <th style={theadThStyle}>Id</th>
+                        <th style={theadThStyle}>Name</th>
+                        <th style={theadThStyle}>RealName</th>
+                        <th style={theadThStyle}>Universe</th>
                     </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>characters.id</td>
-                    <td>characters.realName</td>
-                    <td>characters.actorName</td>
-                    <td>characters.terre</td>
-                </tr>
-                </tbody>
-            </table>
-        </>
+                    </thead>
+                    <tbody>
+                     {characters.map((character,index) => (
+                        <tr key={character.id} style={{
+                            backgroundColor: index % 2 === 0 ? '#3c4a59' : '#424f5e'
+                        }}>
+                            <td style={tbodytdStyle}>{character.id}</td>
+                            <td style={{color: '#7fb069'
+                                ,padding: '12px 16px'
+                                ,fontSize: '14px'
+                                ,fontWeight: '500'
+                            }}>{character.name}</td>
+                            <td style={tbodytdStyle}>{character.realName}</td>
+                            <td style={tbodytdStyle}>{character.universe}</td>
+                        </tr>
+                    ))}
+
+                    </tbody>
+                </table>
+            </section>
+
+        </main>
     )
 }
 
---
+++ TEST/src/back/script.js
@@ -0,0 +1,77 @@
+// PUT /characters/:id ==> Update a character by ID
+// DELETE /characters/:id ==> Delete a character by ID
+
+const express = require('express');
+const { json } = require('express/lib/response');
+const { error } = require('node:console');
+const app = express();
+const fs = require('node:fs/promises')
+
+app.use(express.json())
+
+
+// GET /characters ==> Get all characters
+app.get('/characters', async (req,res) => {
+    try{
+        const data = await fs.readFile('characters.json','utf-8')
+        const characters = JSON.parse(data)
+        res.json(characters.characters)
+    }
+    catch(error){
+        res.status(500).json({error : "Erreur lors de la lecture des characters"})
+    }
+}) 
+
+// GET /characters/:id ==> Get a character by ID
+app.get('/characters/:id', async (req,res) => {
+    try{
+        const data = await fs.readFile('characters.json' , 'utf-8')
+        const characters = JSON.parse(data)
+        const characterId = parseInt(req.params.id)
+        const character = characters.characters.find(char => char.id == characterId)
+
+        if(!character){
+            return res.status(404).json({error : "cannot find the character"})
+        }
+        res.json(character)
+    }
+    catch(err){
+        res.status(500).json({err : "Erreur lors de la lecture du peronnage"})
+    }
+})
+
+// POST /characters ==> Create a new character
+app.post('/charcters', async (req,res) => {
+    try{
+        const data = await fs.readFile('characters.json','utf-8')
+        const characters = JSON.parse(data)
+
+        const {name,realName,Universe} = req.body
+
+        if(!name || !realName || ! Universe){
+            return res.status(400).json({error : "can't be empty"})
+        }
+
+        const newId = Math.max(...characters.characters.map(char => char.id)) + 1
+
+        const newCharacter = {
+            id: newId,
+            name,
+            realName,
+            Universe
+        }
+
+        characters.characters.push(newCharacter)
+        await fs.writeFile('characters.json', JSON.stringify(characters,null,2))
+
+        res.status(200).json(newCharacter)
+    }
+    catch(error){
+        res.status(500).json({error : "cannot create the character"})
+    }
+})
+ 
+app.listen(8080, () => {
+    console.log("Server running on 8080");
+    
+})
